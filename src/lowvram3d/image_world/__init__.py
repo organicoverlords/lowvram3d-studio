3cdaa410bb56f9b2662cc19fd358087c1bdc7d41
organicoverlords <finaldope@gmail.com>
2026-08-02 19:03:50 +0300

    feat: export world-frame and surface-projection APIs

--- src/lowvram3d/image_world/__init__.py
+++ src/lowvram3d/image_world/__init__.py
@@ -13,12 +13,19 @@ from .contracts import (
 from .hydrology import d8_flow_direction, flow_accumulation, priority_flood_fill, stream_mask
 from .profiles import QUALITY_PROFILES, HardwareProfile, QualityProfile, gtx1660_super_6gb
 from .stage_graph import PipelinePlan, StageSpec, build_plan
+from .surface_projection import SurfaceProjectionResult, project_moge_surface, robust_xy_bounds
 from .terrain import (
     CompletedHeightfield,
     HeightfieldObservation,
     complete_heightfield,
     rasterize_point_map,
     slope_degrees,
+)
+from .world_frame import (
+    WorldFrameEstimate,
+    camera_to_world_rotation,
+    estimate_world_up_from_normals,
+    transform_camera_vectors,
 )
 
 __all__ = [
@@ -37,14 +44,21 @@ __all__ = [
     "RouteDecision",
     "SourceImageIdentity",
     "StageSpec",
+    "SurfaceProjectionResult",
+    "WorldFrameEstimate",
     "build_plan",
+    "camera_to_world_rotation",
     "classify_input",
     "complete_heightfield",
     "d8_flow_direction",
+    "estimate_world_up_from_normals",
     "flow_accumulation",
     "gtx1660_super_6gb",
     "priority_flood_fill",
+    "project_moge_surface",
     "rasterize_point_map",
+    "robust_xy_bounds",
     "slope_degrees",
     "stream_mask",
+    "transform_camera_vectors",
 ]
